7bb9ca7679e740f0154732e86d3bbee4d9dfd886
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2024-03-19 11:53:49 +0300

    add variable jlink_package

--- install-jlink.sh
+++ install-jlink.sh
@@ -1,3 +1,5 @@
-wget --post-data 'accept_license_agreement=accepted&non_emb_ctr=confirmed&submit=Download+software' https://www.segger.com/downloads/jlink/JLink_Linux_arm.deb
-sudo dpkg -i JLink_Linux_arm.deb
-rm JLink_Linux_arm.deb
+jlink_package="JLink_Linux_arm.deb"
+
+wget --post-data 'accept_license_agreement=accepted&non_emb_ctr=confirmed&submit=Download+software' https://www.segger.com/downloads/jlink/"$jlink_package"
+sudo dpkg -i "$jlink_package"
+rm "$jlink_package"
